6cfdca681a3b4baa62669249db0826aee04fc81a
claire <claire@DESKTOP-7I82QFE>
2022-01-27 13:27:13 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TechAssignment2_BasicConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello my name is Claire, and my favorite animal is a cat!");
+        }
+    }
+}
